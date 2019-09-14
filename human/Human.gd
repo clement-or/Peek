bd2b6d2e48d505a18dcef9ac85b24c362e6b9edf
Topy <topy72.mine@gmail.com>
2019-09-14 12:24:47 +0200

    Human can move

---
+++ human/Human.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+export var speed = 300 # px/s
+export var jump_speed = 400 # px/s
+export var gravity = 900 #px/s²
+
+"""
+enum states {
+	IDLE,
+	WALK,
+	JUMP,
+	FALL,
+	DIE,
+	SLEEP
+	}
+var state = states.IDLE
+var prev_state
+"""
+
+var velocity = Vector2(0,0)
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	_check_input()
+	velocity.y += gravity*delta
+	$Label.text = String(velocity.y)
+	velocity = move_and_slide(velocity, Vector2.UP, true)
+
+func _check_input():
+	var x_direction = int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
+	velocity.x = lerp(velocity.x, x_direction*speed, 0.5)
+	
+	# Vertical input
+	if (Input.is_action_pressed("ui_up") && is_on_floor()):
+		jump()
+	if (Input.is_action_just_released("ui_up")):
+		stop_jump()
+
+func jump():
+	velocity.y = -jump_speed
+
+func stop_jump():
+	if velocity.y < 0:
+		velocity.y += -0.5*velocity.y
